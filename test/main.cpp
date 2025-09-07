ed7e89496196dbbd313eaaacfaebd387a0fcaa9b
DrBPatch5707 <briar2patch@gmail.com>
2025-09-07 05:38:14 -0500

    Update main.cpp for change to macros

--- test/main.cpp
+++ test/main.cpp
@@ -14,8 +14,8 @@ int main() {
     const std::string file_name = "disk.img";
     int file = open(file_name.c_str(), O_RDWR);
     if(file <= 0)
-        LOG(ERROR, "failed to open file " + file_name);
-        RTLOG(ERROR, "<runtime> failed to open file " + file_name);
+        DLOG(ERROR, "failed to open file " + file_name);
+        LOG(ERROR, "<runtime> failed to open file " + file_name);
         return 1;
 
     close(file);
